ec3d83c5187d66d975a3fdfb8e0ecccb305f28a2
liubiao <liubiao2638@gmail.com>
2017-09-17 18:20:27 +0800

    sin code for dialogue analysis

--- run.sh
+++ run.sh
@@ -9,15 +9,4 @@ max_epochs=30
 
 cmd="python sin.py -validFreq $validFreq -valid_batch_size $valid_batch_size -batch_size $batch_size -dim_proj $dim_proj -n_words $n_words -max_epochs $max_epochs"
 
-if [ $# -eq 0 ]; then
-	#THEANO_FLAGS="mode=FAST_RUN,floatX=float32,exception_verbosity=high" $cmd
-	THEANO_FLAGS="mode=FAST_RUN,floatX=float32" $cmd
-
-elif [ "$1" = "gpu" ]; then
-	export PATH="/usr/local/cuda/bin:${PATH}"
-	export LD_LIBRARY_PATH="/usr/local/cuda/lib:${LD_LIBRARY_PATH}"
-	export LD_LIBRARY_PATH="/usr/local/cuda/lib64:${LD_LIBRARY_PATH}"
-	THEANO_FLAGS="mode=FAST_RUN,device=gpu0,floatX=float32" $cmd
-else
-	echo "error"
-fi
+THEANO_FLAGS="mode=FAST_RUN,floatX=float32" $cmd
